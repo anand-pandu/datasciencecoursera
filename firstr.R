4301b4399737cc48d256cf5481f9685296940dc9
anand-pandu <anand.pandu@gmail.com>
2015-02-18 21:23:25 -0600

    Test 2

--- firstr.R
+++ firstr.R
@@ -1,4 +1,4 @@
-Dog_biscuits=c(0,1,2,3,4,5,6,7,8,9,10)
+Dog_biscuits=c(0,1,2,3,4,5,6,7,8,9,10,12)
 Tail_wags = c(0,0,1,3,8,13,14,12,15,16,14)
 tw=mean(Tail_wags)
 plot(Tail_wags~Dog_biscuits,typ="b",col=3)
